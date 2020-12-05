4f38dba2d6fa64f11079117d210a5ef8f04d1238
Rens Rooimans <github@rensrooimans.nl>
2020-12-05 07:26:21 +0100

    day 5

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -1,26 +1,50 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day05 : ASolution
     {
+        readonly List<string> boardingPassLines = new List<string>();
+        private List<long> seats;
 
-        public Day05() : base(05, 2020, "")
+        public Day05() : base(05, 2020, "Binary Boarding")
         {
-
+            boardingPassLines = Input.SplitByNewline();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            seats = boardingPassLines.Select(CalculateNumber).ToList();
+            return seats.Max().ToString();
+        }
+
+        private long CalculateNumber(string input)
+        {
+            long seat = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[^(i + 1)] is 'B' or 'R')
+                {
+                    seat += 1 << i;
+                }
+            }
+            return seat;
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            seats.Sort();
+
+            for(int i = 1; i < seats.Count; i ++)
+            {
+                if (seats[i - 1] + 1 != seats[i])
+                {
+                    return (seats[i - 1] + 1).ToString();
+                }
+            }
+            return "error";
         }
     }
 }
