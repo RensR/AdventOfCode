87d1c857f35776094629d8d6582073da6fecf504
Rens Rooimans <github@rensrooimans.nl>
2020-12-06 07:20:24 +0100

    Cleanup code with built in Intersect

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -4,10 +4,10 @@ using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day06 : ASolution
     {
-        List<string> lines;
+        readonly List<string> lines;
+
         public Day06() : base(06, 2020, "Custom Customs")
         {
             lines = Input.Split("\n\n").ToList();
@@ -22,15 +22,16 @@ namespace AdventOfCode.Solutions.Year2020
         {
             return lines.Select(line =>
             {
-                var peopleEnumerable = line.Split('\n').Select(q => q.Distinct());
-
-                return peopleEnumerable.Aggregate<IEnumerable<char>, IEnumerable<char>>(
-                    new List<char> { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' } as IEnumerable<char>,
-                    GetMatching).Count();
-
+                return line
+                .Split('\n')
+                .Select(q => q.Distinct())
+                .Aggregate<IEnumerable<char>, IEnumerable<char>>( new List<char> { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' },
+                    (a, b) => a.Intersect(b))
+                .Count();
             }).Sum().ToString();
         }
 
+        // Made own intersect, replaced by built in Intersect
         private static IEnumerable<char> GetMatching(IEnumerable<char> first, IEnumerable<char> second)
         {
             foreach(char f in first)
