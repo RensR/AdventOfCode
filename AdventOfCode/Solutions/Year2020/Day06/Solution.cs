de758d6590bd3a43eb4178501d6263b8107a23ee
Rens Rooimans <github@rensrooimans.nl>
2020-12-06 07:05:20 +0100

    day 6

--- AdventOfCode/Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day06/Solution.cs
@@ -1,26 +1,43 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day06 : ASolution
     {
-
-        public Day06() : base(06, 2020, "")
+        List<string> lines;
+        public Day06() : base(06, 2020, "Custom Customs")
         {
-
+            lines = Input.Split("\n\n").ToList();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return lines.Select(line => line.Replace("\n", "").Distinct().Count()).Sum().ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return lines.Select(line =>
+            {
+                var peopleEnumerable = line.Split('\n').Select(q => q.Distinct());
+
+                return peopleEnumerable.Aggregate<IEnumerable<char>, IEnumerable<char>>(
+                    new List<char> { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm' } as IEnumerable<char>,
+                    GetMatching).Count();
+
+            }).Sum().ToString();
+        }
+
+        private static IEnumerable<char> GetMatching(IEnumerable<char> first, IEnumerable<char> second)
+        {
+            foreach(char f in first)
+            {
+                if (second.Contains(f))
+                    yield return f;
+            }
         }
     }
 }
