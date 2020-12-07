460af57ba6593e9e28662ab8ccb368cb3997c3e6
Rens Rooimans <github@rensrooimans.nl>
2020-12-07 07:47:32 +0100

    day 7

--- AdventOfCode/Solutions/Year2020/Day07/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day07/Solution.cs
@@ -1,26 +1,90 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day07 : ASolution
     {
+        private readonly Dictionary<string, List<string>> isContainedIn = new Dictionary<string, List<string>>();
+        private readonly Dictionary<string, List<string>> contains = new Dictionary<string, List<string>>();
+        readonly Dictionary<string, bool> seen = new Dictionary<string, bool>();
+        int count = -1;
 
-        public Day07() : base(07, 2020, "")
+        public Day07() : base(07, 2020, "Handy Haversacks")
         {
+            var lines = Input.Replace("bags", "bag").Replace(" ", "").Replace(".", "").SplitByNewline();
 
+            foreach(string line in lines)
+            {
+                var segments = line.Split("contain");
+                var innerBags = segments[1].Split(",");
+                var containingBag = segments[0];
+
+                foreach (var innerBag in innerBags)
+                {
+                    // part 1
+                    var cleanInnerBag = Regex.Replace(innerBag, @"\d*", "");
+
+                    if (isContainedIn.ContainsKey(cleanInnerBag)) 
+                    {
+                        isContainedIn[cleanInnerBag].Add(containingBag);
+                    }
+                    else
+                    {
+                        isContainedIn.Add(cleanInnerBag, new List<string> { containingBag });
+                    }
+
+                    // part 2
+                    if (innerBag == "nootherbag") continue;
+                    var numberOfBags = int.Parse(innerBag[0].ToString());
+                    var bagName = innerBag[1..];
+
+                    if (contains.ContainsKey(containingBag))
+                    {
+                        contains[containingBag].AddRange(Enumerable.Repeat(cleanInnerBag, numberOfBags));
+                    }
+                    else
+                    {
+                        contains.Add(containingBag, Enumerable.Repeat(cleanInnerBag, numberOfBags).ToList());
+                    }
+                }
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var current = "shinygoldbag";
+            CheckNextBag(current);
+            return count.ToString();
+        }
+
+        private void CheckNextBag(string current)
+        {
+            if (seen.ContainsKey(current))
+                return;
+            seen.Add(current, true);
+            count++;
+            if (!isContainedIn.ContainsKey(current)) 
+                return;
+            var canBeContainedIn = isContainedIn[current];
+            foreach(var bag in canBeContainedIn)
+            {
+                CheckNextBag(bag);
+            }
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return (CheckBag("shinygoldbag") - 1).ToString();
+        }
+
+        private int CheckBag(string current)
+        {
+            if (!contains.ContainsKey(current))
+                return 1;
+
+            return 1 + contains[current].Sum(CheckBag);
         }
     }
 }
