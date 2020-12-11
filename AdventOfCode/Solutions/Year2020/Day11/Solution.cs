5e76ff9bb71ceef3b75adaf7cce7ef774085ceba
Rens Rooimans <github@rensrooimans.nl>
2020-12-11 08:36:40 +0100

    Day 11

--- AdventOfCode/Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day11/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,132 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day11 : ASolution
     {
-
+        string[] seating;
         public Day11() : base(11, 2020, "")
         {
-
+            seating = Input.SplitByNewline().ToArray();
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var newSeating = seating;
+            var changed = true;
+            while (changed)
+            {
+                (newSeating, changed) = UpdateSeating(newSeating);
+            }
+            return newSeating.Sum(s => s.Count(c => c == '#')).ToString();
+        }
+
+        private (string[], bool) UpdateSeating(string[] currentSeating)
+        {
+            var newSeating = new string[seating.Length];
+            var changed = false;
+            for(int i = 0; i < currentSeating.Length; i++)
+            {
+                var newLine = string.Empty;
+                for (int j = 0; j < currentSeating[i].Length; j++)
+                {
+                    if (currentSeating[i][j] == '.')
+                    {
+                        newLine += '.';
+                        continue;
+                    }
+                    var nextTo = 0;
+                    for(var left = -1; left <= 1; left++)
+                    {
+                        if (i + left < 0 || i + left >= currentSeating.Length) continue;
+                        for (var up = -1; up <= 1; up++)
+                        {
+                            if (left == 0 && up == 0) continue;
+                            if (j + up < 0 || j + up >= currentSeating[i].Length) continue;
+
+                            nextTo += currentSeating[i + left][j + up] == '#' ? 1 : 0;
+                        }
+                    }
+
+                    if (currentSeating[i][j] == 'L')
+                    {
+                        newLine += nextTo == 0 ? '#' : 'L';
+                    }
+                    else if (currentSeating[i][j] == '#')
+                    {
+                        newLine += nextTo >= 4 ? 'L' : '#';
+                    }
+                }
+                newSeating[i] = newLine;
+                if (newSeating[i] != currentSeating[i])
+                    changed = true;
+            }
+
+            return (newSeating, changed);
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var newSeating = seating;
+            var changed = true;
+            while (changed)
+            {
+                (newSeating, changed) = UpdateSeatingB(newSeating);
+            }
+            return newSeating.Sum(s => s.Count(c => c == '#')).ToString();
+        }
+
+        private (string[], bool) UpdateSeatingB(string[] currentSeating)
+        {
+            var newSeating = new string[seating.Length];
+            var changed = false;
+            for (int i = 0; i < currentSeating.Length; i++)
+            {
+                var newLine = string.Empty;
+                for (int j = 0; j < currentSeating[i].Length; j++)
+                {
+                    if (currentSeating[i][j] == '.')
+                    {
+                        newLine += '.';
+                        continue;
+                    }
+                    var nextTo = 0;
+                    for (var left = -1; left <= 1; left++)
+                    {
+                        if (i + left < 0 || i + left >= currentSeating.Length) continue;
+                        for (var up = -1; up <= 1; up++)
+                        {
+                            if (left == 0 && up == 0) continue;
+                            if (j + up < 0 || j + up >= currentSeating[i].Length) continue;
+
+                            var leftleft = left;
+                            var upup = up;
+                            while(currentSeating[i + leftleft][j + upup] == '.')
+                            {
+                                leftleft += left;
+                                upup += up;
+                                if (i + leftleft < 0 || i + leftleft >= currentSeating.Length) break;
+                                if (j + upup < 0 || j + upup >= currentSeating[i].Length) break;
+                                
+                            }
+                            if (i + leftleft < 0 || i + leftleft >= currentSeating.Length) continue;
+                            if (j + upup < 0 || j + upup >= currentSeating[i].Length) continue;
+                            nextTo += currentSeating[i + leftleft][j + upup] == '#' ? 1 : 0;
+                        }
+                    }
+
+                    if (currentSeating[i][j] == 'L')
+                    {
+                        newLine += nextTo == 0 ? '#' : 'L';
+                    }
+                    else if (currentSeating[i][j] == '#')
+                    {
+                        newLine += nextTo >= 5 ? 'L' : '#';
+                    }
+                }
+                newSeating[i] = newLine;
+                if (newSeating[i] != currentSeating[i])
+                    changed = true;
+            }
+
+            return (newSeating, changed);
         }
     }
 }
