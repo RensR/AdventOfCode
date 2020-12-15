f301aaead508244768dafad76d336b39401580ac
Rens Rooimans <github@rensrooimans.nl>
2020-12-15 08:00:04 +0100

    Day 15

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -1,26 +1,62 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day15 : ASolution
     {
+        readonly Dictionary<int, int> LastSpoken = new Dictionary<int, int>();
+        int previous;
+        int i = 0;
 
         public Day15() : base(15, 2020, "")
         {
+            var numbers = Input.Split(',').ToList().Select(int.Parse).ToArray();
 
+            for(i = 1; i <= numbers.Length; i++)
+            {
+                if(i == numbers.Length)
+                {
+                    previous = numbers[i - 1];
+                }
+                else
+                {
+                    LastSpoken.Add(numbers[i - 1], i);
+                }
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+           return SolveFor(2020).ToString();
         }
 
+        private int SolveFor(int turn)
+        {
+            for (; i <= turn; i++)
+            {
+                if (!LastSpoken.ContainsKey(previous))
+                {
+                    LastSpoken[previous] = i - 1;
+                    previous = 0;
+                    continue;
+                }
+                var lastIndex = LastSpoken[previous];
+
+                var diff = i - 1 - lastIndex;
+                LastSpoken[previous] = i - 1;
+                previous = diff;
+            }
+
+
+            return previous;
+        }
+
+        // Because 30000000 > 2020 and part two runs after part one we don't have to reset anything
+        // and can start at 2020 for the solution vor part 2.
         protected override string SolvePartTwo()
         {
-            return null;
+            return SolveFor(30000000).ToString();
         }
     }
 }
