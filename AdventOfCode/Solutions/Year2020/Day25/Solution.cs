07b5b84b5642bbe81376ffd7676f79bf8f9fa322
Rens Rooimans <github@rensrooimans.nl>
2020-12-25 12:49:05 +0100

    Day 25

--- AdventOfCode/Solutions/Year2020/Day25/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day25/Solution.cs
@@ -1,23 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace AdventOfCode.Solutions.Year2020
 {
-
     class Day25 : ASolution
     {
-
-        public Day25() : base(25, 2020, "")
+        private readonly long _door;
+        private readonly long _keyCard;
+        
+        public Day25() : base(25, 2020, "Combo Breaker")
         {
-
+            var lines = Input.SplitByNewline();
+            _door = long.Parse(lines[0]);
+            _keyCard = long.Parse(lines[1]);
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var val = 1;
+            for (var i = 1;; i++)
+            {
+                val = val * 7 % 20201227;
+
+                if (val == _door)
+                {
+                    return Loop(_keyCard, i).ToString();
+                }
+
+                if (val == _keyCard)
+                {
+                    return Loop(_door, i).ToString();
+                }
+            }
         }
 
+        private static long Loop(long subjectNumber, long loopSize)
+        {
+            long number = 1;
+            for (long i = 1; i <= loopSize; i++)
+            {
+                number = number * subjectNumber % 20201227;
+            }
+
+            return number;
+        }
+        
         protected override string SolvePartTwo()
         {
             return null;
