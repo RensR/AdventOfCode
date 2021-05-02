a7713bbf2899474ae5647efbb155a1e9d3a074b3
Rens Rooimans <github@rensrooimans.nl>
2021-05-02 20:44:56 +0200

    chore: integrate answer 01 b within the 01 file  in y2018

--- AdventOfCode/Solutions/2018/01/main.go
+++ AdventOfCode/Solutions/2018/01/main.go
@@ -19,7 +19,11 @@ func main(){
 	}
 
 	freq := strings.Split(string(dat), "\r\n")
+	computeA(freq)
+	computeB(freq)
+}
 
+func computeA(freq []string){
 	currentFreq := 0
 
 	for _, change := range freq {
@@ -27,5 +31,23 @@ func main(){
 		currentFreq += number
 	}
 
-	fmt.Print(currentFreq)
+	fmt.Printf("answer A: %d\n",  currentFreq)
+}
+
+func computeB(freq []string){
+	currentFreq := 0
+
+	dict := make(map[int]bool)
+	dict[0] = true
+	for true{
+		for _, change := range freq {
+			number, _ := strconv.Atoi(change)
+			currentFreq += number
+			if dict[currentFreq]{
+				fmt.Printf("answer B: %d\n",  currentFreq)
+				return
+			}
+			dict[currentFreq] = true
+		}
+	}
 }
