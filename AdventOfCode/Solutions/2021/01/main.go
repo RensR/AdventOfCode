970754c6adb33c632a3cf6d3efb74240a7c2bca8
Rens Rooimans <github@rensrooimans.nl>
2021-11-30 20:56:30 +0100

    chore: prep 2021

---
+++ AdventOfCode/Solutions/2021/01/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/kindermoumoute/adventofcode/pkg"
+	"github.com/kindermoumoute/adventofcode/pkg/execute"
+)
+
+// returns part1 and part2
+func run(input string) (interface{}, interface{}) {
+	list := pkg.ParseIntList(input, "\n")
+
+	part1, part2 := 0, list[0]
+
+	//// 2D lib
+	//
+	//m := twod.NewMapFromInput(input)
+	//m:= make( twod.Map)
+	//twod.NewPoint()
+	////
+
+	return part1, part2
+}
+
+func parse(s string) {
+	lines := strings.Split(s, "\n")
+	for _, line := range lines {
+		pkg.MustScanf(line, "")
+	}
+	return
+}
+
+func main() {
+	execute.Run(run, nil, puzzle, true)
+}
---
+++ AdventOfCode/Solutions/2021/01/puzzle.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/kindermoumoute/adventofcode/pkg/execute"
+)
+
+var tests = execute.TestCases{
+	{
+		puzzle,
+		``,
+		``,
+	},
+}
+
+var puzzle = `1`
