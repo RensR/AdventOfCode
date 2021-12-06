1703365a06af9fc88c1fe5c855bded9c0a9161b5
Rens Rooimans <github@rensrooimans.nl>
2021-12-06 10:10:09 +0100

    chore: cleanup & speedup day 6

--- AdventOfCode/Solutions/2021/06/main.go
+++ AdventOfCode/Solutions/2021/06/main.go
@@ -6,9 +6,12 @@ import (
 )
 
 // returns part1 and part2
-func run(input string) (interface{}, interface{}) {
-	ages := parse(input)
-	a1 := 0
+func run(input string) (a1 interface{}, a2 interface{}) {
+	ages := make([]int, 9)
+
+	for _, num := range pkg.ParseIntList(input, ",") {
+		ages[num]++
+	}
 
 	for i := 0; i < 256; i++ {
 		if i == 80 {
@@ -20,28 +23,14 @@ func run(input string) (interface{}, interface{}) {
 	return a1, countFish(ages)
 }
 
-func countFish(ages map[int]int) (result int) {
+func countFish(ages []int) (result int) {
 	for _, age := range ages {
 		result += age
 	}
 	return result
 }
 
-func parse(s string) map[int]int {
-	nums := pkg.ParseIntList(s, ",")
-	ages := make(map[int]int)
-	for i := 0; i < 9; i++ {
-		ages[i] = 0
-	}
-
-	for _, num := range nums {
-		ages[num]++
-	}
-
-	return ages
-}
-
-func passDay(ages map[int]int) map[int]int {
+func passDay(ages []int) []int {
 	dueFish := ages[0]
 	for i := 1; i < 9; i++ {
 		ages[i-1] = ages[i]
